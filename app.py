1d160ad3124af66027f50e2a0633fa1f579b81df
NayeliSalinas <nayelisalinascontreras97@gmail.com>
2025-05-15 17:38:05 -0500

    Add files via upload

---
+++ app.py
@@ -0,0 +1,25 @@
+from flask import Flask, request, jsonify, render_template
+from flask_cors import CORS
+from modelo import predecir_tiempo
+
+app = Flask(__name__)
+CORS(app)  # Esto habilita CORS para todas las rutas
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/predecir', methods=['POST'])
+def predecir():
+    data = request.json
+    distancia = float(data['distancia'])
+    velocidad = float(data['velocidad'])
+    trafico = int(data['trafico'])
+    clima = int(data['clima'])
+    terreno = int(data['terreno'])
+
+    resultado = predecir_tiempo(distancia, velocidad, trafico, clima, terreno)
+    return jsonify({'tiempo_estimado': resultado})
+
+if __name__ == '__main__':
+    app.run(debug=True)
